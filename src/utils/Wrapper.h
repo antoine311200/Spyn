db2b74d9fad2b261108f36928f17e4a6e53a7f6a
antoine311200 <antoine311200@gmail.com>
2023-04-29 13:04:47 +0200

    🧷 Wrapper for global hierarchy upon better copying and storing mechanism

---
+++ src/utils/Wrapper.h
@@ -0,0 +1,34 @@
+#ifndef WRAPPER_H
+#define WRAPPER_H
+
+template<class T>
+class Wrapper
+{
+public:
+	Wrapper(T* p = 0) : _ptr(p) {}
+	Wrapper(const T& rhs) { _ptr = rhs.clone(); }
+	Wrapper(const Wrapper<T>& rhs) { if (rhs._ptr != 0) _ptr = rhs._ptr->clone(); else _ptr = 0; }
+
+	~Wrapper() { if (_ptr) delete _ptr; }
+
+	Wrapper& operator=(const Wrapper<T>& rhs)
+	{
+		if (this != &rhs)
+		{
+			if (_ptr != 0) delete _ptr;
+			_ptr = (rhs._ptr != 0) ? rhs._ptr->clone() : 0;
+		}
+		return *this;
+	}
+
+	T& operator*() { return *_ptr; }
+	T* operator->() { return _ptr; }
+
+	const T& operator*() const { return *_ptr; }
+	const T* const operator->() const { return _ptr; }
+
+private:
+	T* _ptr;
+};
+
+#endif
